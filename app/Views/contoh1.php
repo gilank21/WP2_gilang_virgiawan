542e3ce97fc3b1498f47521ea63e126ba0a6a2a2
gilank21 <gilangargoem@gmail.com>
2023-03-21 13:54:25 +0700

    upload

---
+++ app/Views/contoh1.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>Membuat Biodata Sederhana</title>
+</head>
+<body style="background-color:silver">
+<table border="1" style="width: 700px;">
+<tr><td align="center" colspan="3" bgcolor="red"><font color="blue" size="20px">
+<b>BIODATA</b>
+</font></td></tr>
+<tr>
+<td rowspan="5" width="100px">
+<img src="image.jpg" width="150px" height="200px" border="2">
+</td>
+<td bgcolor="#0000FF">NAMA </td>
+<td> Gilang virgiawan </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> UMUR </td>
+<td> ##### </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> ALAMAT </td>
+<td> Bogor barat </td>
+</tr>
+<tr>
+<td bgcolor="#0000FF"> HOBI </td>
+<td> olahraga </td>
+</tr>
+<tr>
+<td align="center" colspan="2" bgcolor="white"> <a href="##"></a> </td>
+</tr>
+</table>
+</body>
+</html> 
